ec148a386289fd0cf1b0e5eb9b0008413b706390
dajacobs <dajacobs.tab@gmail.com>
2016-01-11 14:59:54 -0600

    descriptions comment

---
+++ game.pl
@@ -0,0 +1,17 @@
+/*
+	Text descriptions of all the palces in the game.
+*/
+description(valley, 
+	'You are in a pleasant valley, with a trail ahead.').
+description(path, 
+	'You are on a path, with ravines on both sides.').
+description(cliff,
+	'You are teetering on the edge of a cliff.').
+description(fork,
+	'You are at a fork in the path.').
+description(maze(_),
+	'You are in a maze of twisty trails, all alike.').
+description(mountaintop,
+	'You are on the mountaintop.').
+description(labyrinth(_),
+	'You are lost in a winding corridor.').
