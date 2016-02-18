021e801427ab799055d5ec168ec10e197ffe299b
dajacobs <dajacobs.tab@gmail.com>
2016-02-18 15:06:56 -0600

    minor edit

--- c++/game.cpp
+++ c++/game.cpp
@@ -12,12 +12,11 @@ double average = win/loss;
 int position = -1;
 string direction;
 string ogre[552];
-string trease[552];
+string treasure[552];
 string move[] = {"forward.", "left.", "right."};
 int node[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
 string location[] = {"Trail", "Cliff", "Fork", "Mountaintop", "Maze 0", "Maze 1", "Maze 2", "Maze 3", "Maze 4", "Labyrinth 0", "Labyrinth 1", "Labyrinth 2", "Labyrinth 3", "Labyrinth 4", "Labyrinth 5", "Labyrinth 6", "Labyrinth 7", "Labyrinth 8", "Labyrinth 9", "Labyrinth 10", "Labyrinth 11", "Labyrinth 12", "Labyrinth 13", "Labyrinth 14", "Labyrinth 15"};
 string description[] = {"You are on a path, with ravines on both sides.", "You are in a pleasant valley, with a trail ahead.", "You are teetering on the edge of a cliff.", "You are at a fork in the path.", "You are in a maze of twisty trails, all alike.", "You are on the mountaintop.", "You are lost in a winding corridor."};
-vector <vector <string> > data;
 
 //Prototypes
 int locate();
@@ -34,6 +33,7 @@ int locate() {
 }
 //Read configuration file
 void readConf(string file) {
+	vector <vector <string> > data;
 	ifstream infile(file);
 	while(infile) {
 		string s;
@@ -445,9 +445,8 @@ void run() {
 		cout << "Average Wins: " << average;
 	}
 }
-//Main function
-int main(){
-	void go() {
+//Go function
+void go() {
 		readConf("configurations.txt");
 		readComm("commands_v1.txt", 1);
 		readComm("commands_v2.txt", 2);
@@ -468,5 +467,8 @@ int main(){
 			run();
 		}
 	}
+//Main function
+int main() {
+	go();
 	return 0;
 }
