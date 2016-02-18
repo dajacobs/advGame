7b7f340593d6d418dae4c34514c67b22878b6ccf
dajacobs <dajacobs.tab@gmail.com>
2016-02-18 14:05:32 -0600

    prototypes

---
+++ c/game.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototypes
+int getLoc();
+void readConf(char *file[]);
+void readComm(char *file[], inv v);
+char *report[](int loc);
+void move(char *dir[]);
+void run();
+void go();
